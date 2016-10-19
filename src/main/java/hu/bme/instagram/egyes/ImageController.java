61d6466ef24bf3337dd41b9c7b85e375be2c73d9
Paróczi Gergő <paroczi@gmail.com>
2016-10-20 01:16:19 +0200

    Google autentikáció implementálása

--- src/main/java/hu/bme/instagram/egyes/ImageController.java
+++ src/main/java/hu/bme/instagram/egyes/ImageController.java
@@ -1,41 +1,109 @@
 package hu.bme.instagram.egyes;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Map;
+
 
 @Controller
 public class ImageController {
 
     private Photo photo;
+    private User user;
+    //    Google API clien id for google sign-in auth2
+    private final static String CLIENT_ID = "941751993774-vcefv09ou5poadotds1e0clvsma43qjd.apps.googleusercontent.com";
 
     private Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
             "cloud_name", "egyesrepo-cloudinary",
             "api_key", "525216556637445",
             "api_secret", "PtK7Je6XE8rEVCqRdWcDk_KzTFs"));
 
+    @RequestMapping("/")
+    public String index() {
+        if (user == null) {
+            return "redirect:signin";
+        } else {
+            return "index";
+        }
+    }
+
+    @GetMapping("/signin")
+    public String signInGet() {
+        return "signin";
+    }
+
+    @PostMapping("/signin")
+    public String singIn(@RequestParam(value = "idtoken", required = true) String idTokenString) throws GeneralSecurityException, IOException {
+
+        JsonFactory jsonFactory = new JacksonFactory();
+        NetHttpTransport transport = new NetHttpTransport();
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                .setAudience(Arrays.asList(CLIENT_ID))
+                // If you retrieved the token on Android using the Play Services 8.3 API or newer, set
+                // the issuer to "https://accounts.google.com". Otherwise, set the issuer to
+                // "accounts.google.com". If you need to verify tokens from multiple sources, build
+                // a GoogleIdTokenVerifier for each issuer and try them both.
+                .setIssuer("accounts.google.com")
+                .build();
+
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+        if (idToken != null) {
+            Payload payload = idToken.getPayload();
+
+            // User infók eltárolása
+            user = new User();
+            user.setUserId(payload.getSubject());
+            user.setName((String) payload.get("name"));
+            user.setPictureUrl((String) payload.get("picture"));
+
+
+        } else {
+            System.out.println("Invalid ID token.");
+            return "signin";
+        }
+
+        return "upload";
+        // TODO: 2016-10-20 itt valamiért nem visz át az upload.html-re
+    }
+
 
     @GetMapping("/upload")
     public String uploadingForm(Model model) {
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+            model.addAttribute("userImage", user.getPictureUrl());
+        } else {
+            return "redirect:signin";
+        }
         return "upload";
     }
 
     @PostMapping("/upload")
     public String uploadingSubmit(@RequestParam(value = "image", required = true) MultipartFile uploadedPhoto,
-                                 @RequestParam(value = "title", required = true) String title,
-                                 Model model) {
+                                  @RequestParam(value = "title", required = true) String title,
+                                  Model model) {
 
-        if (uploadedPhoto != null && !uploadedPhoto.isEmpty()) {
+        if (uploadedPhoto.getSize() <= 5000000) {
 
             try {
                 Map uploadResult;
@@ -45,7 +113,7 @@ public class ImageController {
 
                 Map options = ObjectUtils.asMap(
                         "public_id", "temp/" + new SimpleDateFormat("yyyy-MM-dd hh-mm-ss").format(new Date()) + "-" + title,
-                        "transformation",transformation
+                        "transformation", transformation
                 );
 
 
@@ -55,22 +123,27 @@ public class ImageController {
                 photo.setPublic_id((String) uploadResult.get("public_id"));
                 photo.setUrl((String) uploadResult.get("url"));
 
+//                ha sikeres a képfeltöltés, akkor tovább küldjök a result page-re
+                model.addAttribute("details", photo.getPublic_id());
+
+                model.addAttribute("photo", cloudinary.url()
+                        .transformation(new Transformation().width(100).height(150).crop("fill"))
+                        .imageTag(photo.getPublic_id())
+                );
+
+                return "result";
+
 
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("Hiba a feltöltésnél:" + e.getMessage());
+                return "upload";
             }
 
+        } else {
+            System.out.println("A fájl mérete nagyobb a megengedett 5MB-nál.");
+            return "upload";
         }
 
-        model.addAttribute("details", photo.getPublic_id());
-
-        model.addAttribute("photo", cloudinary.url()
-                .transformation(new Transformation().width(100).height(150).crop("fill"))
-                .imageTag(photo.getPublic_id())
-        );
-
-        return "result";
     }
-
 
 }
---
+++ src/main/java/hu/bme/instagram/egyes/User.java
@@ -0,0 +1,31 @@
+package hu.bme.instagram.egyes;
+
+public class User {
+    private String userId;
+    private String name;
+    private String pictureUrl;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+}
