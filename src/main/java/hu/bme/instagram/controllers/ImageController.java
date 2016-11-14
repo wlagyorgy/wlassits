cab695e07b9c21491ea2869d2689ce0a302bd43c
GergoMuller <bejla94@gmail.com>
2016-11-14 15:06:19 +0100

    nem transzformal feltoltésnél

--- src/main/java/hu/bme/instagram/controllers/ImageController.java
+++ src/main/java/hu/bme/instagram/controllers/ImageController.java
@@ -76,7 +76,6 @@ public class ImageController {
         photoRepository.save(photo);
 
         redirectAttributes.addFlashAttribute("photo", cloudinary.url()
-                .transformation(new Transformation().width(100).height(150).crop("fill"))
                 .imageTag(photo.getPublic_id()));
         addAttributesToModel(model, photo);
 
