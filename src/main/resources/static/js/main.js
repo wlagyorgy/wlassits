eb94bd423ac6a21f20f8ab8378c07de12923c7b2
ommhoa <omar.osman.asd@gmail.com>
2016-11-22 22:19:17 +0100

    ****** frontend magic ******: hozzányúlni csak saját felelősségre! képfeltöltésnél kiírja a file nevét tallózás után.

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,20 @@
+$(document).on('change', ':file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready( function() {
+    $(':file').on('fileselect', function(event, numFiles, label) {
+        console.log(numFiles);
+        console.log(label);
+        var filename = document.createElement("LABEL");
+        filename.innerHTML = label;
+        var span = document.getElementById("uploadFileName");
+        if (span.firstChild) {
+            span.removeChild(span.firstChild);
+        }
+        span.appendChild(filename);
+    });
+});
