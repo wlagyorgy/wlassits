42a154627b0a7a12015184c3ae8f9d9a484545b9
ommhoa <omar.osman.asd@gmail.com>
2016-11-22 23:40:16 +0100

    refaktor: PhotoComparator mostmár az ImageController belső osztálya

--- src/main/java/hu/bme/instagram/controllers/ImageController.java
+++ src/main/java/hu/bme/instagram/controllers/ImageController.java
@@ -207,4 +207,18 @@ public class ImageController {
         }
     }
 
+    class PhotoComparator implements Comparator<Photo> {
+
+        @Override
+        public int compare(Photo p1, Photo p2) {
+            if(p1.getCreated_at().after(p2.getCreated_at()))
+                return -1;
+            else if(p1.getCreated_at().before(p2.getCreated_at()))
+                return 1;
+            else
+                return 0;
+        }
+
+    }
+
 }
